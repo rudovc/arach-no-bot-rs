4114bf4d26750eb0ba9bfec45ae66720aba364b7
Roko Udovč <rudovc@profico.hr>
2023-07-30 11:38:20 +0200

    Improve testing (it's not good now either, though)

--- src/handlers/message.rs
+++ src/handlers/message.rs
@@ -1,120 +1,153 @@
 use crate::constants;
-use crate::staging;
-use anyhow::anyhow;
-use anyhow::Result;
+use crate::staging::is_testing_channel;
+use color_eyre::eyre::eyre;
+use color_eyre::Result;
 
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{CacheHttp, EmojiId, Guild, Message, ReactionType};
 
-#[derive(Debug)]
-pub struct Message<'a> {
-    channel_id: &'a u64,
-    content: &'a String,
-    message: Option<&'a serenity::Message>,
-}
-
-impl<'a> From<&'a Message<'_>> for Message<'a> {
-    fn from(message: &'a Message) -> Message<'a> {
-        Message {
-            channel_id: message.channel_id,
-            content: message.content,
-            message: message.message,
-        }
-    }
-}
-
-impl<'a> From<&'a serenity::Message> for Message<'a> {
-    fn from(message: &'a serenity::Message) -> Message<'a> {
-        Message {
-            channel_id: message.channel_id.as_u64(),
-            content: &message.content,
-            message: Some(message),
-        }
-    }
-}
-
-fn scan_for_r_dtg(content: &str) -> Result<bool, regex::Error> {
+fn check_string_for_r_drg(content: &str) -> Result<bool, regex::Error> {
     Ok(regex::Regex::new(constants::R_DTG_REGEX)?.is_match(content))
 }
 
-pub async fn handle<'a>(
-    message: impl Into<Message<'a>>,
-    ctx: Option<
-        impl serenity::CacheHttp
-            + std::convert::AsRef<poise::serenity_prelude::Cache>
-            + std::convert::AsRef<poise::serenity_prelude::Http>,
-    >,
+fn check_message_content_in_channel_for_r_dtg(
+    channel_id: u64,
+    content: &str,
+    testing: bool,
 ) -> Result<bool> {
-    let Message {
-        channel_id,
-        content,
-        message,
-    } = message.into();
-
-    let is_testing_channel = staging::is_testing_channel(match message {
-        Some(message) => message.channel_id,
-        None => serenity::ChannelId::from(*channel_id),
-    });
-
-    match (*channel_id, is_testing_channel) {
+    match (channel_id, testing) {
         (constants::channels::DENSITY_THE_GAME_ID, false) | (_, true) => {
-            let has_match = scan_for_r_dtg(content)?;
-
-            if let (true, Some(message), Some(ctx)) = (has_match, message, ctx) {
-                let guild: serenity::Guild = message
-                    .guild(&ctx)
-                    .ok_or_else(|| anyhow!("Did not find guild for message id: {}.", message.id))?;
-
-                let emoji = if is_testing_channel {
-                    serenity::ReactionType::Unicode("🤖".to_owned())
-                } else {
-                    serenity::ReactionType::from(
-                        guild
-                            .emoji(&ctx, serenity::EmojiId::from(constants::HMM_EMOJI_ID))
-                            .await?,
-                    )
-                };
-                message.react(&ctx, emoji).await?;
-            }
+            let has_match = check_string_for_r_drg(content)?;
 
             Ok(has_match)
         }
-        _ => Err(anyhow!(
+        _ => Err(eyre!(
             "Channel ID {channel_id} is not in monitored channel list."
         )),
     }
+}
+
+pub async fn handle<'a>(
+    message: Message,
+    ctx: impl CacheHttp
+        + std::convert::AsRef<poise::serenity_prelude::Cache>
+        + std::convert::AsRef<poise::serenity_prelude::Http>,
+) -> Result<()> {
+    let Message { channel_id, .. } = message;
+
+    let is_testing_channel = is_testing_channel(message.channel_id);
+    let result = check_message_content_in_channel_for_r_dtg(
+        channel_id.into(),
+        &message.content,
+        is_testing_channel,
+    )?;
+
+    if result {
+        let guild: Guild = message
+            .guild(&ctx)
+            .ok_or_else(|| eyre!("Did not find guild for message id: {}.", message.id))?;
+
+        let emoji = if is_testing_channel {
+            ReactionType::Unicode("🤖".to_owned())
+        } else {
+            ReactionType::from(
+                guild
+                    .emoji(&ctx, EmojiId::from(constants::HMM_EMOJI_ID))
+                    .await?,
+            )
+        };
+
+        message.react(&ctx, emoji).await?;
+    }
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn test_handler_only_read_messages_in_specific_channels() {
-        let mut test_message = Message {
-            channel_id: &0,
-            content: &String::from("Irrelevant content"),
-            message: None,
-        };
+    use test_case::test_case;
 
-        let result = handle(&test_message, None::<serenity::Context>).await;
-
-        assert!(result.is_err_and(|e| e.to_string().contains("is not in monitored channel list")));
-
-        test_message.channel_id = &constants::channels::DENSITY_THE_GAME_ID;
-
-        let result = handle(&test_message, None::<serenity::Context>).await;
-
-        assert!(!result.unwrap());
+    enum Expect {
+        Result(bool),
+        Error(),
     }
 
-    #[test]
-    fn test_flag_messages_with_r_dtg() {
-        let test_content = "Does not include the Bad Word";
+    #[test_case(
+        constants::channels::DENSITY_THE_GAME_ID,
+        Expect::Result(false),
+        "Does not include the Bad Word!",
+        false ;
+        "when there's no mention of r/dtg"
+    )]
+    #[test_case(
+        constants::channels::DENSITY_THE_GAME_ID,
+        Expect::Result(true),
+        "Includes the Bad Word! https://www.reddit.com/r/DestinyTheGame/",
+        false ;
+        "when there's mention of r/dtg"
+    )]
+    #[test_case(
+        0,
+        Expect::Error(),
+        "Includes the Bad Word! https://www.reddit.com/r/DestinyTheGame/",
+        false ;
+        "when a message is sent outside of the destiny channel"
+    )]
+    #[test_case(
+        constants::channels::DENSITY_THE_GAME_ID,
+        Expect::Result(true),
+        "Includes the Bad Word! https://www.reddit.com/r/DestinyTheGame/",
+        true ;
+        "when a matching message is sent in the testing channel"
+    )]
+    #[test_case(
+        constants::channels::DENSITY_THE_GAME_ID,
+        Expect::Result(false),
+        "Does not include the Bad Word!",
+        true ;
+        "when a non-matching message is sent in the testing channel"
+    )]
+    #[tokio::test]
+    async fn test_handler_only_read_messages_in_specific_channels(
+        channel_id: u64,
+        expect: Expect,
+        content: &str,
+        testing: bool,
+    ) {
+        match expect {
+            Expect::Result(expect) => {
+                let result =
+                    check_message_content_in_channel_for_r_dtg(channel_id, content, testing);
+                assert_eq!(
+                    result.expect("Result should not error when provided a valid channel."),
+                    expect
+                );
+            }
+            Expect::Error() => {
+                let result =
+                    check_message_content_in_channel_for_r_dtg(channel_id, content, testing);
+                assert!(result
+                    .is_err_and(|e| e.to_string().contains("is not in monitored channel list")));
+            }
+        };
+    }
 
-        assert!(!scan_for_r_dtg(test_content).unwrap());
-
-        let test_content = "Includes the Bad Word! https://www.reddit.com/r/DestinyTheGame/";
-
-        assert!(scan_for_r_dtg(test_content).unwrap())
+    #[test_case(
+        "Does not include the Bad Word",
+        false ;
+        "when there's no mention of r/dtg"
+    )]
+    #[test_case(
+        "Includes the Bad Word! https://www.reddit.com/r/DestinyTheGame/",
+        true ;
+        "when there's a regular www r/dtg link"
+    )]
+    #[test_case(
+        "Includes the Bad Word! https://old.reddit.com/r/DestinyTheGame/%E2%9C%93",
+        true ;
+        "when there's an old reddit r/dtg link"
+    )]
+    fn test_flag_messages_with_r_dtg(test_content: &str, expect: bool) {
+        assert_eq!(check_string_for_r_drg(test_content).unwrap(), expect);
     }
 }
--- src/handlers/reaction.rs
+++ src/handlers/reaction.rs
@@ -1,34 +1,47 @@
 use crate::constants;
-use crate::database::{self as database, workaround::WithPutRequest};
-use crate::types::ReactionInteraction;
-use crate::types::UserRecord;
-use anyhow::{anyhow, Result};
-use poise::serenity_prelude as serenity;
+use crate::database::{workaround::WithPutRequest, Database, FirebaseIncrement, IncrementMap};
+use crate::types::{ReactionInteraction, UserRecord};
+use color_eyre::{eyre::eyre, Result};
+use poise::serenity_prelude::{Cache, CacheHttp, Http, Reaction, ReactionType, UserId};
+
+fn match_haha_emoji(emoji_id: &u64) -> Result<()> {
+    if !constants::HAHA_EMOJI_IDS.contains(emoji_id) {
+        return Err(eyre!("Emoji does not match haha emoji."));
+    }
+    Ok(())
+}
+
+fn check_for_self_reaction(
+    reaction_giver_id: Option<UserId>,
+    message_author_id: UserId,
+) -> Result<()> {
+    match reaction_giver_id {
+        Some(id) if id == message_author_id => {
+            Err(eyre!("User tried to haha react their own message."))
+        }
+        Some(_) => Ok(()),
+        None => Err(eyre!("Expected to find user ID, found None")),
+    }
+}
+
+fn get_firebase_increment_for_reaction(reaction_interaction: ReactionInteraction) -> IncrementMap {
+    match reaction_interaction {
+        ReactionInteraction::Add => FirebaseIncrement::increment_by(1),
+        ReactionInteraction::Remove => FirebaseIncrement::increment_by(-1),
+    }
+}
 
 pub async fn handle<'a>(
     reaction_interaction: ReactionInteraction,
-    ctx: Option<
-        impl serenity::CacheHttp
-            + std::convert::AsRef<poise::serenity_prelude::Cache>
-            + std::convert::AsRef<poise::serenity_prelude::Http>,
-    >,
-    database: &database::Database,
+    reaction: &Reaction,
+    ctx: impl CacheHttp + std::convert::AsRef<Cache> + std::convert::AsRef<Http>,
+    database: &Database,
 ) -> Result<()> {
-    let reaction: &serenity::Reaction = (&reaction_interaction).into();
-
-    if let (Some(ctx), serenity::ReactionType::Custom { id: emoji_id, .. }) =
-        (&ctx, &reaction.emoji)
-    {
+    if let ReactionType::Custom { id: emoji_id, .. } = &reaction.emoji {
         let message = reaction.message(ctx).await?;
 
-        if !constants::HAHA_EMOJI_IDS.contains(emoji_id.as_u64())
-            || reaction
-                .user_id
-                // We're not going to allow cheating
-                .is_some_and(|reaction_giver_id| reaction_giver_id == message.author.id)
-        {
-            return Ok(());
-        }
+        match_haha_emoji(emoji_id.as_u64())?;
+        check_for_self_reaction(reaction.user_id, message.author.id)?;
 
         let user = database
             .connection
@@ -37,35 +50,94 @@ pub async fn handle<'a>(
 
         let user_record = UserRecord::new(&message.author.name, None);
 
-        match reaction_interaction {
-            ReactionInteraction::Add(_) => {
-                user.update(&user_record)
-                    .await
-                    .map_err(|err| anyhow!(err))?;
+        user.update(&user_record).await.map_err(|err| eyre!(err))?;
 
-                let increment = &database::FirebaseIncrement::increment_by(1);
+        let increment = get_firebase_increment_for_reaction(reaction_interaction);
 
-                user.at("haha_count")
-                    .put(increment)
-                    .await
-                    .map_err(|err| anyhow!(err))?;
-            }
-            ReactionInteraction::Remove(_) => {
-                user.update(&user_record)
-                    .await
-                    .map_err(|err| anyhow!(err))?;
-
-                let increment = &database::FirebaseIncrement::increment_by(-1);
-
-                user.at("haha_count")
-                    .put(increment)
-                    .await
-                    .map_err(|err| anyhow!(err))?;
-            }
-        };
+        user.at("haha_count")
+            .put(&increment)
+            .await
+            .map_err(|err| eyre!(err))?;
     }
 
     Ok(())
 }
 
-// TODO: Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::ReactionInteraction;
+    use std::collections::HashMap;
+    use test_case::test_case;
+
+    #[test_case(0, false ; "when the emoji does not match a known haha")]
+    #[test_case(
+        931317049395937320, true ;
+        "when the emoji matches the local known haha"
+    )]
+    #[test_case(
+        599772724029685760, true ;
+        "when the emoji matches an external haha"
+    )]
+    #[test_case(
+        1092619586706341989, true ;
+        "when the emoji matches another external haha"
+    )]
+    fn test_matching_a_haha_emoji(emoji_id: u64, expect: bool) {
+        let result = match_haha_emoji(&emoji_id);
+
+        assert_eq!(result.is_ok(), expect)
+    }
+
+    #[test_case(
+        Some(UserId::from(0)), UserId::from(1), true ; "when the user IDs don't match"
+    )]
+    #[test_case(Some(UserId::from(4799853325)), UserId::from(6381533619), true)]
+    #[test_case(Some(UserId::from(9100277759)), UserId::from(9316850468), true)]
+    #[test_case(Some(UserId::from(7480516711)), UserId::from(6780785561), true)]
+    #[test_case(
+        Some(UserId::from(1)), UserId::from(1), false ; "when the user IDs do match"
+    )]
+    #[test_case(Some(UserId::from(7132173634)), UserId::from(7132173634), false)]
+    #[test_case(Some(UserId::from(2061429558)), UserId::from(2061429558), false)]
+    #[test_case(Some(UserId::from(2100720041)), UserId::from(2100720041), false)]
+    #[test_case(Some(UserId::from(1)), UserId::from(1), false)]
+    #[test_case(
+        None, UserId::from(1), false ; "when the reaction giver user IDs doesn't exist"
+    )]
+    fn test_self_reaction_filter(
+        reaction_giver_id: Option<UserId>,
+        message_author_id: UserId,
+        expect: bool,
+    ) {
+        let result = check_for_self_reaction(reaction_giver_id, message_author_id);
+
+        assert_eq!(result.is_ok(), expect);
+
+        match (reaction_giver_id, &result) {
+            (Some(_), Err(err)) => {
+                assert!(err
+                    .to_string()
+                    .contains("User tried to haha react their own message."));
+            }
+            (None, Err(err)) => {
+                assert!(err
+                    .to_string()
+                    .contains("Expected to find user ID, found None"));
+            }
+            _ => (),
+        }
+    }
+
+    #[test_case(ReactionInteraction::Add, HashMap::from([(".sv".to_owned(), FirebaseIncrement::new(1))]) ; "when a reaction is added" )]
+    #[test_case(ReactionInteraction::Remove, HashMap::from([(".sv".to_owned(), FirebaseIncrement::new(-1))]) ; "when a reaction is removed" )]
+    fn test_create_firebase_increment_object_for_interaction(
+        reaction_interaction: ReactionInteraction,
+        expect: IncrementMap,
+    ) {
+        assert_eq!(
+            get_firebase_increment_for_reaction(reaction_interaction),
+            expect
+        )
+    }
+}
